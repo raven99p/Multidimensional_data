fa27bf4fcd691f9337c0e59231f828315d6dfd54
sphantom99 <58749624+sphantom99@users.noreply.github.com>
2021-01-08 22:33:09 +0200

    Fixed reading from file and general UI

--- bloom.py
+++ bloom.py
@@ -1,5 +1,6 @@
 from bitarray import bitarray 
 import mmh3
+import numpy as np
 k = 4 
 m = 2415459
 p = 0.1
@@ -34,6 +35,41 @@ class bloom:
 		elif counter == k:
 			return True
 
-bloom1 = bloom(2415459)
-bloom1.add("paulinho")
-print(bloom1.check("paulisdf"))
+
+with open("words.txt") as f:
+	cont = f.read()
+cont = cont.split()
+print(len(cont))
+n = int(len(cont))
+m = (-(n * np.log(p))/(np.log(2)**2))
+m = int(round(m))
+print(m)
+#(1 - (1 - (1/m))**(k*n))**k
+print(p)
+bloom1 = bloom(m)
+for elem in cont:
+	bloom1.add(str(elem))
+print("File read...\n")
+
+choice = 0
+while choice!=3:
+	choice = int(input("What would you like to do now ? :\n 1) Add another Element \n2) Check if word is in the filter \n3) Exit \n"))
+	if choice == 1:
+		word = str(input("Please give me the word you want to store :\n"))
+		bloom1.add(word)
+		n = n + 1
+		print("The word has been added!")
+		pass
+	if choice == 2:
+		word = str(input("Please give me the word you want to search for :\n"))
+		if bloom1.check(word):
+			print("The word is contained in the filter with a probability of p = %f \n"%((1 - (1 - (1/m))**(k*n))**k))
+		else:
+			print("The word is definetly not contained in the filter \n")
+	if choice == 3:
+		print("Thanks for playing!! \n")
+		pass
+
+	pass
+
+
